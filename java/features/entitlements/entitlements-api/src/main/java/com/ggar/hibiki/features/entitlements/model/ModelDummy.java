e26bcff3d27f8a38aabd93aba2783ac55ff7d46e
Gabriel García Fernández <g.gar.fern@hotmail.com>
2026-03-09 16:41:58 +0100

    create module

--- java/features/entitlements/entitlements-api/src/main/java/com/ggar/hibiki/features/entitlements/model/ModelDummy.java
+++ java/features/entitlements/entitlements-api/src/main/java/com/ggar/hibiki/features/entitlements/model/ModelDummy.java
@@ -3,5 +3,4 @@ package com.ggar.hibiki.features.entitlements.model;
 /**
  * Dummy class for domain models.
  */
-public final class ModelDummy {
-}
+public final class ModelDummy {}
--- java/features/entitlements/entitlements-api/src/main/java/com/ggar/hibiki/features/entitlements/pipeline/PipelineDummy.java
+++ java/features/entitlements/entitlements-api/src/main/java/com/ggar/hibiki/features/entitlements/pipeline/PipelineDummy.java
@@ -3,5 +3,4 @@ package com.ggar.hibiki.features.entitlements.pipeline;
 /**
  * Dummy class for pipelines.
  */
-public final class PipelineDummy {
-}
+public final class PipelineDummy {}
